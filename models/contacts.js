1549d825296d53f3bec0a3781fd47744b3cc9336
Rostyslaw Kostyk <rkostyk64@gmail.com>
2022-08-01 21:56:25 +0300

    scheme contacts

--- models/contacts.js
+++ models/contacts.js
@@ -1,62 +1,52 @@
-const fs = require('fs/promises');
-const path = require('path');
-const {v4} = require('uuid');
+const {Schema, model} = require("mongoose");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const Joi=require("Joi");
 
-const updateContactJson = async (contacts)=> {
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-}
+const contactSchema = Schema({
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  });
 
-const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(data);
-  return contacts;
-}
+  const Contact = model("contact", contactSchema);//название - существительное в единственном числе с большой буквы
 
-const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  const result = contacts.find(item => item.id === contactId);
-  if(!result){
-    return null;
+  const contactsAddSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+    favorite: Joi.bool(),
+  });
+  
+  const contactUpdateSchema = Joi.object({
+    name: Joi.string(),
+    email: Joi.string().email(),
+    phone: Joi.string(),
+    favorite: Joi.bool(),
+  })
+    .min(1)
+    .max(4);
+
+  const contactUpdateFavoriteSchema=Joi.object({
+    favorite:Joi.bool().required(),
+  });
+
+  module.exports = {
+    Contact,
+    schemas:{
+      contactsAddSchema,
+      contactUpdateSchema,
+      contactUpdateFavoriteSchema,
+    }
+   
   };
-  return result;
-}
-
-const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const deleteContact = contacts.find(item => item.id === contactId);
-  if(!deleteContact) {
-    return null;
-  };
-  const newContacts = contacts.filter(item => item.id !== contactId);
-  await updateContactJson(newContacts);
-  return deleteContact;
-}
-
-const addContact = async ( name, email, phone ) => {
-  const contacts = await listContacts();
-  const newContact = { id: v4(), name, email, phone };
-  contacts.push(newContact);
-  await updateContactJson(contacts);
-  return newContact;
-}
-
-const updateContact = async (id, body) => {
-  const contacts = await listContacts();
-  const idx = contacts.findIndex(item => item.id === id);
-  if(idx === -1){
-    return null;
-  }
-  contacts[idx] = {...body, id};
-  await updateContactJson(contacts);
-  return contacts[idx];
-}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
