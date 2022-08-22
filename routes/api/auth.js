c2930581c7c782c2b3dfc6c65a2ef47c07f36b2f
Rostyslaw Kostyk <rkostyk64@gmail.com>
2022-08-22 16:34:12 +0300

    auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,92 +1,83 @@
 const express = require("express");
+const createError = require("http-errors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const {v4} = require("uuid");
+
+const {User, schemas} = require("../../models/user");
+const {sendMail} = require("../../helpers");
 
 const router = express.Router();
 
-const createError = require("http-errors");
+const {SECRET_KEY} = process.env;
 
-const bcryptjs = require("bcryptjs");
-
-const jwt = require("jsonwebtoken");
-
-const { User, schemas } = require("./../../models/user");
-
-const { SECRET_KEY } = process.env;
-
-router.post("/signup", async (req, res, next) => {
+// /signup
+router.post("/register", async(req, res, next)=> {
     try {
-        const { error } = schemas.registerJoiSchema.validate(req.body);
-
-        if (error) {
-            throw createError(400, "Ошибка от Joi или другой библиотеки валидации");
-        };
-        const { email, password } = req.body;
-
-        const user = await User.findOne({ email });
-        if (user) {
-            throw createError(409, "Email in use");
-        };
-
-        const salt = await bcryptjs.genSalt(10);
-        const hashPassword = await bcryptjs.hash(password, salt);
-
-        await User.create({
-            email,
-            password: hashPassword
-        });
-
+        const {error} = schemas.register.validate(req.body);
+        if(error){
+            throw new createError(400, error.message)
+        }
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(user){
+            throw new createError(409, "Email in use");
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+        const avatarURL = gravatar.url(email);
+        const verificationToken = v4();
+        await User.create({email, password: hashPassword, verificationToken, avatarURL});
+        const mail = {
+            to: email,
+            subject: "Подтвеждение email",
+            html: `<a target="_blank" href='http://localhost:3000/api/users/${verificationToken}'>Нажмите чтобы подтвердить свой email</a>`
+        }
+        await sendMail(mail);
         res.status(201).json({
             user: {
-                email,
-                "subscription": "starter"
+                email
             }
-        });
+        })
     } catch (error) {
-        next(error);
+        next(error)
     }
 });
 
-router.post("/login", async (req, res, next) => {
+// /signin
+router.post("/login", async(req, res, next)=> {
     try {
-        const { error } = schemas.registerJoiSchema.validate(req.body);
-
-        if (error) {
-            throw createError(400, "Ошибка от Joi или другой библиотеки валидации");
-        };
-        const { email, password } = req.body;
-
-        const user = await User.findOne({ email });
-
-        if (!user) {
-            throw createError(401, "Not authorized");
-        };
-
-        const compareResult = await bcryptjs.compare(password, user.password);
-
-        if (!compareResult) {
-            throw createError(401, "Email or password is wronghorized");
-        };
-        const payload = {
-            id: user._id,
+        const {error} = schemas.register.validate(req.body);
+        if(error){
+            throw new createError(400, error.message)
         }
-
-        const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
-
-        await User.findByIdAndUpdate(user._id, { token });
-
-        res.status(200).json({
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if(!user) {
+            throw new createError(401, "Email or password is wrong");
+        }
+        if(!user.verify) {
+            throw new createError(401, "Email not verify");
+        }
+        const compareResult = await bcrypt.compare(password, user.password);
+        if(!compareResult){
+            throw new createError(401, "Email or password is wrong");
+        }
+        const payload = {
+            id: user._id
+        }
+        const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "1h"});
+        await User.findByIdAndUpdate(user._id, {token});
+        res.json({
             token,
             user: {
-                email,
-                subscription: "starter"
+                email
             }
-        });
-
+        })
     } catch (error) {
-        next(error)
-    };
-});
-
-
-
+        next(error);
+    }
+})
 
 module.exports = router;
