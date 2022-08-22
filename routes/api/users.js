0976ab53cc3f07c83d5c3fc4b05983d10796ed1c
Rostyslaw Kostyk <rkostyk64@gmail.com>
2022-08-22 16:37:21 +0300

    usrs

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,23 +1,86 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs/promises");
+const createError = require("http-errors");
+const Joi = require("joi");
+
+const { User, schemas } = require("../../models/user");
+const { authenticate, upload } = require("../../middlewares")
+const {sendMail} = require("../../helpers");
 
 const router = express.Router();
 
-const { User } = require("../../models/user");
+router.get("/verify/:verificationToken", async(req, res, next)=> {
+    try {
+        const {verificationToken} = req.params;
+        const user = await User.findOne({verificationToken});
+        if(!user){
+            throw createError(404);
+        }
+        await User.findByIdAndUpdate(user._id, {verify: true, verificationToken: ""});
+        res.json({
+            message: 'Verification successful'
+        })
+    } catch (error) {
+        next(error);
+    }
+})
 
-const { authenticate } = require("../../middlewares/index");
+router.post("/verify", async(req, res, next)=> {
+    try {
+        const {error} = schemas.verify.validate(req.body);
+        if(error){
+            throw createError(400, "missing required field email")
+        };
+        const {email} = req.body;
+        const user = await User.findOne({email});
+        if(user.verify){
+            throw createError(400, "Verification has already been passed")
+        }
+        const mail = {
+            to: email,
+            subject: "Подтвеждение email",
+            html: `<a target="_blank" href='http://localhost:3000/api/users/${user.verificationToken}'>Нажмите чтобы подтвердить свой email</a>`
+        }
+        sendMail(mail);
+        res.json({
+            "message": "Verification email sent"
+        })
+    } catch (error) {
+        next(error);
+    }
+})
 
 router.get("/current", authenticate, async (req, res, next) => {
     res.json({
-        email: req.user.email,
-    });
-    next();
+        email: req.user.email
+    })
 });
 
 router.get("/logout", authenticate, async (req, res, next) => {
-        const { id } = req.user;
-        await User.findByIdAndUpdate(id, { token: null });
-        res.status(204).send();
-});
+    const { _id } = req.user;
+    await User.findByIdAndUpdate(_id, { token: "" });
+    res.status(204).send()
+})
 
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+router.patch("/avatars", authenticate, upload.single("avatar"), async(req, res, next)=> {
+    const {_id} = req.user;
+    const {path: tempUpload, filename} = req.file;
+    try {
+        const [extention] = filename.split(".").reverse();
+        const newFileName = `${_id}.${extention}`;
+        const resultUpload = path.join(avatarsDir, newFileName);
+        await fs.rename(tempUpload, resultUpload);
+        const avatarURL = path.join("avatars", newFileName);
+        await User.findByIdAndUpdate(_id, {avatarURL});
+        res.json({
+            avatarURL
+        })
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = router;
